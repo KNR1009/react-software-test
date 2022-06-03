10af344db2507ac79d630af49c6c5c308d64518e
伊東和成 <itokazunari5375@icloud.com>
2022-06-04 08:53:29 +0900

    環境構築

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import { Counter } from "./features/counter/Counter";
+import { Render } from "./Render";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Render></Render>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Render.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Render = () => {
+  return (
+    <>
+      <h1>テストの練習</h1>
+      <input type="text" />
+      <button>CLick1</button>
+      <button>CLick2</button>
+      <p>こんばんわ</p>
+    </>
+  );
+};
+
+export default Render;
