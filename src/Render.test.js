bf0fe2161d8887715b05e273981a962d59f82f7a
伊東和成 <itokazunari5375@icloud.com>
2022-06-04 09:40:40 +0900

    sample test

--- src/Render.test.js
+++ src/Render.test.js
@@ -1,3 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Render from "./Render";
+
+describe("rendering", () => {
+  it("Should render the elements correctly", () => {
+    render(<Render />);
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+  });
+});
