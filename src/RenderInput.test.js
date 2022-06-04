69f0a7f4357dfa95ab346c6f512234525cfd979d
伊東和成 <itokazunari5375@icloud.com>
2022-06-04 13:21:38 +0900

    テストケースの追加

--- src/RenderInput.test.js
+++ src/RenderInput.test.js
@@ -10,3 +10,35 @@ describe("Rendering", () => {
     expect(screen.getByPlaceholderText("Enter")).toBeTruthy();
   });
 });
+
+describe("input form onChange event", () => {
+  it("should update input value correctly", async () => {
+    render(<RenderInput></RenderInput>);
+    const inputValue = screen.getByPlaceholderText("Enter");
+    await userEvent.type(inputValue, "test");
+    expect(inputValue.value).toBe("test");
+  });
+});
+
+describe("console button", () => {
+  /* stateが空の場合は呼び出されない */
+  it("should not trigger output function", async () => {
+    /* propsで送られてくる想定のmock関数 */
+    const outputConsole = jest.fn();
+    render(<RenderInput outputConsole={outputConsole}></RenderInput>);
+    await userEvent.click(screen.getByRole("button"));
+    expect(outputConsole).not.toHaveBeenCalled();
+  });
+  /* stateにデータが入っている場合はpropsの関数を呼び出し */
+  it("should trigger output function", async () => {
+    /* propsで送られてくる想定のmock関数 */
+    const outputConsole = jest.fn();
+    render(<RenderInput outputConsole={outputConsole}></RenderInput>);
+    const inputValue = screen.getByPlaceholderText("Enter");
+    /* 入力フォームにデータを入れることでstateの値が変わる */
+    await userEvent.type(inputValue, "test");
+    /* stateにデータが入っている場合はpropsの関数を呼び出し */
+    await userEvent.click(screen.getByRole("button"));
+    expect(outputConsole).toHaveBeenCalledTimes(1);
+  });
+});
